cf2d29caff5d11664124e4d1102a347c25924a94
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-01-28 11:11:22 +0000

    implemented sqlparsing print in home view

--- sqlquery/views.py
+++ sqlquery/views.py
@@ -4,14 +4,23 @@ from .models import Product
 from django.http import JsonResponse
 from django.core.serializers import serialize
 from django.db import connection
+from pygments import highlight
+from pygments.formatters import TerminalFormatter
+from pygments.lexers.sql import SqlLexer
+from sqlparse import format
 
 
-# Create your views here.
 def Home(request):
     qs = Product.objects.all()
     serialized_data = serialize("json", qs)
-    
-    print(connection.queries)
+
+    queries = list(connection.queries)
+
+    for sq in queries:
+        sqlformatted = format(str(sq["sql"]), reindent=True)
+        sqlformatted = highlight(sqlformatted, SqlLexer(), TerminalFormatter())
+        print(sqlformatted)
+
     serialized_data = json.loads(serialized_data)
 
     return JsonResponse(serialized_data, safe=False, status=200)
