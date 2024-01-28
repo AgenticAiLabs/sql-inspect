8b19bf9e5221d439a379e48b59419a89b4cbb434
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-01-28 19:26:56 +0000

    implementation of sqlparse middleware

--- sqlanalyzer/settings.py
+++ sqlanalyzer/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "sqlquery.middleware.new_middleware"
 ]
 
 ROOT_URLCONF = "sqlanalyzer.urls"
--- sqlquery/views.py
+++ sqlquery/views.py
@@ -3,24 +3,16 @@ from django.shortcuts import render
 from .models import Product
 from django.http import JsonResponse
 from django.core.serializers import serialize
-from django.db import connection
-from pygments import highlight
-from pygments.formatters import TerminalFormatter
-from pygments.lexers.sql import SqlLexer
-from sqlparse import format
 
 
 def Home(request):
     qs = Product.objects.all()
+
     serialized_data = serialize("json", qs)
 
-    queries = list(connection.queries)
-
-    for sq in queries:
-        sqlformatted = format(str(sq["sql"]), reindent=True)
-        sqlformatted = highlight(sqlformatted, SqlLexer(), TerminalFormatter())
-        print(sqlformatted)
-
+    ww = Product.objects.all()
+    ss = serialize("json", ww)
+    
     serialized_data = json.loads(serialized_data)
 
     return JsonResponse(serialized_data, safe=False, status=200)
