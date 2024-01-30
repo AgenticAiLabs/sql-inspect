dc721877fd969e73db83a837941df9d7cd2658ec
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-01-30 14:39:14 +0000

    testing impl: http endpoint and middleware logging

--- package/tests/demo_app/views.py
+++ package/tests/demo_app/views.py
@@ -3,6 +3,11 @@ from .models import Product
 
 
 def test_endpoint(request):
-    qs = Product.objects.all()
+    query_count = request.GET.get("query_count", 1)
+
+    qs = None
+    for i in range(int(query_count)):
+        qs = Product.objects.all()
+        qs = qs.count()
 
     return HttpResponse(qs)
